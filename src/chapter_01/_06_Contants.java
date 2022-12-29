d84bd27d5cde744047989c8d8c6aa53eaacf1665
jueon <wndjs803@naver.com>
2022-12-29 16:22:31 +0900

    Constnats

---
+++ src/chapter_01/_06_Contants.java
@@ -0,0 +1,12 @@
+package chapter_01;
+
+public class _06_Contants {
+    public static void main(String[] args) {
+        final String KR_CONTURY_CODE = "+82";
+        System.out.println(KR_CONTURY_CODE);
+
+        final double PI = 3.141592;
+        final String DATE_OF_BIRTH = "2000-01-01";
+
+    }
+}
