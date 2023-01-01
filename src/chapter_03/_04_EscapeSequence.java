c33ea4a5abcb7829918986759083f46f557d2c0d
jueon <wndjs803@naver.com>
2023-01-01 15:42:08 +0900

    EscapeSequence

---
+++ src/chapter_03/_04_EscapeSequence.java
@@ -0,0 +1,12 @@
+package chapter_03;
+
+public class _04_EscapeSequence {
+    public static void main(String[] args) {
+        // 특수문자
+        // \n(줄바꿈), \t(탭키), \\(\), \"("), \'(')
+        System.out.println("I like 'java'"); // ""(큰 따옴표) 안에 '(작은 따옴표)사용해서 표시 가능
+
+        char c = '\'';
+        System.out.println(c);
+    }
+}
