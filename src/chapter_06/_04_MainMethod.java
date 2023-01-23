ae8a4547ce2ffc79217cb7b017bcc27f31543c28
jueon <wndjs803@naver.com>
2023-01-23 17:14:00 +0900

    just Update

---
+++ src/chapter_06/_04_MainMethod.java
@@ -0,0 +1,9 @@
+package chapter_06;
+
+public class _04_MainMethod {
+    public static void main(String[] args) {
+        for (String s: args) {
+            System.out.println(s);
+        }
+    }
+}
